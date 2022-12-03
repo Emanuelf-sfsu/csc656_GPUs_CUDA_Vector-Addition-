80a10bb63d1c35976075fe0f7bd73602119b712f
Emanuel Francis <efrancis1@mail.sfsu.edu>
2022-12-02 16:43:37 -0800

    time check

--- vecadd_cpu.cpp
+++ vecadd_cpu.cpp
@@ -1,42 +1,53 @@
 #include <iostream>
 #include <math.h>
+#include <chrono>
 
+// function to add the elements of two arrays
 void add(int n, float *x, float *y)
 {
-    for(int i=0; i<n; i++){
+    for (int i = 0; i < n; i++)
+    {
         y[i] = x[i] + y[i];
     }
 }
 
 int main(void)
 {
-    int N  = 1 <<24; 
+    int N = 1 << 24;
 
     float *x = new float[N];
     float *y = new float[N];
 
-    //initalize x and y arrays on the host
-    for(int i = 0; i<N; i++){
+    // initalize x and y arrays on the host
+    for (int i = 0; i < N; i++)
+    {
         x[i] = 1.0f;
         y[i] = 2.0f;
     }
 
-    //Run kernel on 1M elements on the CPU
-    add(N, x, y);
+    // insert your timer code here
+    std::chrono::time_point<std::chrono::high_resolution_clock> start_time = std::chrono::high_resolution_clock::now();
 
-    //Check for errors (all values should be 3.0f)
+    // Run kernel on 1M elements on the CPU
+    add(N, x, y);
+    // insert your end timer code here, and print out elapsed time for this problem size
+    std::chrono::time_point<std::chrono::high_resolution_clock> end_time = std::chrono::high_resolution_clock::now();
+
+    // Check for errors (all values should be 3.0f)
     float maxError = 0.0f;
-    
-    for(int i = 0;i < N; i++)
+
+    for (int i = 0; i < N; i++)
     {
-        maxError = fmax(maxError, fabs(y[i]-3.0f));
+        maxError = fmax(maxError, fabs(y[i] - 3.0f));
     }
 
-    std::cout << "Max error" << maxError << std::endl;
+    std::cout << "Max error " << maxError << std::endl;
+    std::chrono::duration<double> elapsed = end_time - start_time;
+    std::cout << "Elapsed time is : " << elapsed.count() << " " << std::endl;
 
-    //Free Memory
-    delete [] x;
-    delete [] y;
+    // Free Memory
+    delete[] x;
+    delete[] y;
 
     return 0;
 }
